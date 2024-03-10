aed6245779b5dd77a51811a9b7e3fc067116597f
tiobi <tiobi@naver.com>
2024-03-10 17:53:41 +0900

    onboarding page

---
+++ lib/features/onboarding/presentation/pages/on_boarding_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('OnBoarding Page'),
+      ),
+    );
+  }
+}
